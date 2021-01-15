b8dfcfa9b38b957e1d7ac309e4ef200af6e1676e
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2021-01-16 03:34:16 +0700

    ADD global variable user useContext

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React/* , { useContext } */ from 'react'
+import AuthenticatedUser from '../components/AuthenticatedUser';
+import { /* UserContext, */ UserProvider } from './context/User'
+
+/* Cara 1 
+const User = () => {
+    const {user} = useContext(UserContext)
+
+    return (
+        <div>
+            My Name is: {user.name}
+        </div>
+    )
+}
 
 function Home() {
+
     return (
         <div className="container">
-            Home
+            <UserProvider>
+                <User />
+            </UserProvider>
+        </div>
+    );
+}
+ */
+
+//  cara 2
+function Home() {
+    /* const {user} = useContext(UserContext) */
+
+    return (
+        <div className="container">
+            <UserProvider>
+                <div className="d-flex">
+                    <div className="mr-1">My Name is </div><AuthenticatedUser /> 
+                </div>
+            </UserProvider>
         </div>
     );
 }
